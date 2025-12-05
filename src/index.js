58d081234f13caae84977e33eef81fab6a4a4e19
Shannen <78643060+shannenlolol@users.noreply.github.com>
2025-12-05 09:47:42 +0800

    Initialize basic HTTP server in index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import http from "http";
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello from Jenkins demo Node app\n");
+});
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
